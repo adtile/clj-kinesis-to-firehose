d4ccbeb7442296a0dfcfc5b321eff76c7019d700
Jaakko Suutarla <jaakko@adtile.me>
2015-11-05 11:15:28 +0200

    Test also error cases

--- test/kinesis_to_firehose/core_test.clj
+++ test/kinesis_to_firehose/core_test.clj
@@ -1,20 +1,13 @@
 (ns kinesis-to-firehose.core-test
   (:require [clojure.test :refer :all]
+            [clj-containment-matchers.clojure-test :refer :all]
             [base64-clj.core :refer [encode]]
             [cheshire.core :refer [parse-string generate-string]]
-            [kinesis-to-firehose.core :refer [parse-failures send-request kinesis->firehose!]])
+            [kinesis-to-firehose.core :as core :refer [send-to-firehose kinesis->firehose!]]
+            [kinesis-to-firehose.test-helper :refer [events->kinesis-stream]])
   (:import [com.amazonaws.services.kinesisfirehose.model PutRecordBatchResult PutRecordBatchResponseEntry Record]
            [java.nio ByteBuffer]
            [java.io ByteArrayInputStream ByteArrayOutputStream]))
-
-(def kinesis-template (parse-string (slurp "test/resources/kinesis-template.json") true))
-
-(defn events->kinesis-stream [events]
-  (let [compile-kinesis-data (fn [event] (assoc-in kinesis-template [:kinesis :data] (encode (generate-string event))))
-        records {"Records" (map compile-kinesis-data events)}]
-    (-> (generate-string records)
-        (.getBytes)
-        (ByteArrayInputStream.))))
 
 (def sample-event-one
   {:type "session"
@@ -26,13 +19,25 @@
    :id 234
    :some {:bar "foo"}})
 
-(def sample-event-three
-  {:type "open"
+(def sample-event-unkown
+  {:type "unknown"
    :id 345
    :some {:foo "bar"}})
 
-(defn- fake-request [expected-data request]
-  (is (= (-> request .getRecords first .getData .array String.) expected-data))
+(def test-mappings
+  {:dispatch (fn [event] (:type (parse-string event true)))
+   :rules
+   [{:name :sessions
+     :dispatch-value #{"session"}
+     :transform (fn [event] (generate-string event))
+     :streams ["stream://1"]}
+    {:name :tracking-events
+     :dispatch-value #{"clicks" "open" "foobar"}
+     :transform (fn [event] (generate-string event))
+     :streams ["stream://4"]}]})
+
+
+(defn- fake-request [request]
   (doto (PutRecordBatchResult.)
     (.setFailedPutCount (int 0))))
 
@@ -43,33 +48,42 @@
                              (.setErrorCode "666")
                              (.setErrorMessage "Unknown"))
                            (PutRecordBatchResponseEntry.)])))
-(def test-mappings
-  {:dispatch (fn [event] (:type (parse-string event true)))
-   :rules
-   [{:name :sessions
-     :dispatch-value #{"session"}
-     :transform (fn [event] (generate-string event))
-     :streams ["stream://1" "stream://2"]}
-    {:name :tracking-events
-     :dispatch-value #{"clicks" "open" "foobar"}
-     :transform (fn [event] (generate-string event))
-     :streams ["stream://3" "stream://4"]}]})
 
-(def expected-payload
-  "{\"type\":\"session\",\"id\":123,\"some\":{\"foo\":\"bar\"}}")
 
 (deftest sends-record-from-kinesis-to-firehose
-  (with-redefs [send-request (partial fake-request expected-payload)]
-    (let [input-stream (events->kinesis-stream [sample-event-one])]
-      (is (= {:sessions {:succeeded 1 :failed 0} :skipped 0}
+  (with-redefs [core/send-request fake-request]
+    (let [input-stream (events->kinesis-stream [sample-event-one sample-event-one])]
+      (is (equal? {:sessions {:succeeded 2 :failed 0} :skipped 0}
              (kinesis->firehose! input-stream test-mappings))))))
 
-;(deftest throws-error-when-all-puts-fail
-;  (with-redefs [send-request first-one-failing-request]
-;    (is (thrown? Exception (handle-batch sample)))))
+(deftest send-record-from-kinesis-to-firehose-and-ignore-unknown-message
+  (with-redefs [core/send-request fake-request]
+    (let [input-stream (events->kinesis-stream [sample-event-one sample-event-one sample-event-unkown])]
+      (is (equal? {:sessions {:succeeded 2 :failed 0} :skipped 1}
+             (kinesis->firehose! input-stream test-mappings))))))
 
-;(deftest logs-failed-records-in-case-of-partial-success
-;  (with-redefs [send-request first-one-failing-request]
-;    (is (= (handle-batch first-one-failing-sample)
-;           {:succeeded-count 1
-;            :failed [{:payload "Failing" :error-code "666" :error-msg "Unknown"}]}))))
+(deftest throws-error-when-all-puts-fail
+  (with-redefs [core/send-request first-one-failing-request]
+    (let [input-stream (events->kinesis-stream [sample-event-one])]
+      (is (thrown? Exception (kinesis->firehose! input-stream test-mappings))))))
+
+(deftest logs-failed-records-in-case-of-partial-success
+  (with-redefs [core/send-request first-one-failing-request]
+    (let [input-stream (events->kinesis-stream [sample-event-one sample-event-one])]
+      (is (equal? (kinesis->firehose! input-stream test-mappings)
+                  {:sessions {:succeeded 1
+                              :failed [{:payload "{\"type\":\"session\",\"id\":123,\"some\":{\"foo\":\"bar\"}}" :error-code "666" :error-msg "Unknown"}]}
+                   :skipped 0})))))
+
+(defn send-to-firehose-fake [expected events stream]
+  (is (equal? (vec events) expected)))
+
+(def expected-payload
+  ["{\"type\":\"session\",\"id\":123,\"some\":{\"foo\":\"bar\"}}"
+   "{\"type\":\"session\",\"id\":123,\"some\":{\"foo\":\"bar\"}}"])
+
+(deftest sends-record-from-kinesis-to-firehose-in-correct-format
+  (with-redefs [send-to-firehose (partial send-to-firehose-fake expected-payload)]
+    (let [input-stream (events->kinesis-stream [sample-event-one sample-event-one])]
+      (kinesis->firehose! input-stream test-mappings))))
+
